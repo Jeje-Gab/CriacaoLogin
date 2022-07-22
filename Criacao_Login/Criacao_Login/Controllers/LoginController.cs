e885a3f4ed818ff26836d295d75e931dacd4677b
Gabriel de Jesus <gabriel.jesus@unimedcco.unimedchapeco.com.br>
2022-07-22 17:44:17 -0300

    Adicao de um Sistema de Login

---
+++ Criacao_Login/Criacao_Login/Controllers/LoginController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Criacao_Login.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                return Json(new { Msg = "Usuario Já Logado!"});
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logar(string username, string senha)               
+        {
+            MySqlConnection mySqlConnection = new MySqlConnection("server=localhost;database=usuariodb;uid=root;password=1234");          
+            await mySqlConnection.OpenAsync();
+
+            MySqlCommand mySqlCommand = mySqlConnection.CreateCommand();
+            mySqlCommand.CommandText = $"SELECT * FROM usuarios WHERE usarname = '{username}' AND senha = '{senha}'";                                             
+            
+            MySqlDataReader reader = mySqlCommand.ExecuteReader();
+
+            if (await reader.ReadAsync())
+            {
+                int usuarioId = reader.GetInt32(0);
+                string nome = reader.GetString(1);
+
+                List<Claim> direitosAcesso = new List<Claim>
+                { 
+                    new Claim(ClaimTypes.NameIdentifier, usuarioId.ToString()),
+                    new Claim(ClaimTypes.Name, nome)
+
+
+                };
+
+                var identity = new ClaimsIdentity(direitosAcesso, "Identity.Login");
+                var userPrincipal = new ClaimsPrincipal(new[] { identity });
+
+                await HttpContext.SignInAsync(userPrincipal,
+                    new AuthenticationProperties
+                    { 
+                        IsPersistent = false,
+                        ExpiresUtc = DateTime.Now.AddHours(1)
+                    });
+
+                return Json(new { Msg = "Usuario Logado com sucesso!"});                
+            }
+
+            return Json(new { Msg = "Usuario não encontrado verifique suas credenciais" });        
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            if(User.Identity.IsAuthenticated)
+            {
+                await HttpContext.SignOutAsync();
+            }
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
+
+
+
+
+
+
